5f3eb503c20df995fcd7e0d5cbc9bdb7ee98f085
KTPL-0116 <lakhan.rathi@kpoint.com>
2022-06-19 22:05:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/codelandassignment/MainActivity.java
@@ -0,0 +1,254 @@
+package com.example.codelandassignment;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    String url = "";
+    ImageView image;
+    Button button,share;
+    EditText urltext;
+    ProgressDialog mProgressDialog;
+    private int EXTERNAL_STORAGE_PERMISSION_CODE = 23;
+    final static int APP_STORAGE_ACCESS_REQUEST_CODE = 501; // Any value
+    private String filePatrh = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.image);
+        button = findViewById(R.id.button);
+        urltext = findViewById(R.id.textView);
+        url = String.valueOf(urltext.getText());
+        new DownloadImage().execute(url);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                url = String.valueOf(urltext.getText());
+
+                new DownloadImage().execute(url);
+                new SaveWallpaperAsync(getApplicationContext()).execute(url);
+
+            }
+        });
+
+        share = findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(filePatrh!=null){
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("image/jepg");
+                shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:/" + filePatrh));
+                startActivity(Intent.createChooser(shareIntent, "Share image"));
+                }else {
+
+                    Toast.makeText(getApplicationContext(), "Download the image first", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+
+
+        if (checkPermission()) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, Uri.parse("package:" + BuildConfig.APPLICATION_ID));
+        startActivityForResult(intent, APP_STORAGE_ACCESS_REQUEST_CODE);
+        }
+
+    }
+
+    private boolean checkPermission() {
+
+        return ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                ;
+    }
+
+
+    private class DownloadImage extends  AsyncTask<String, String, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("Download Image Tutorial");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+        @Override
+        protected Bitmap doInBackground(String... URL) {
+            String imageURL = URL[0];
+            Bitmap bitmap = null;
+            try {
+                // Download Image from URL
+                InputStream input = new java.net.URL(imageURL).openStream();
+                // Decode Bitmap
+                bitmap = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            // Set the bitmap into ImageView
+            image.setImageBitmap(result);
+            // Close progressdialog
+            mProgressDialog.dismiss();
+        }
+    }
+
+
+    public class SaveWallpaperAsync extends AsyncTask<String, String, String> {
+        private Context context;
+        private ProgressDialog pDialog;
+        String image_url;
+        URL ImageUrl;
+        String myFileUrl1;
+        Bitmap bmImg = null;
+
+        public SaveWallpaperAsync(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+
+            super.onPreExecute();
+
+//            pDialog = new ProgressDialog(context);
+//            pDialog.setMessage("Please wait...");
+//            pDialog.setIndeterminate(false);
+//            pDialog.setCancelable(false);
+//            pDialog.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+
+            InputStream is = null;
+
+            try {
+
+                ImageUrl = new URL(args[0]);
+                // myFileUrl1 = args[0];
+
+                HttpURLConnection conn = (HttpURLConnection) ImageUrl
+                        .openConnection();
+                conn.setDoInput(true);
+                conn.connect();
+                is = conn.getInputStream();
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inPreferredConfig = Bitmap.Config.RGB_565;
+                bmImg = BitmapFactory.decodeStream(is, null, options);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+
+                String path = ImageUrl.getPath();
+                String idStr = path.substring(path.lastIndexOf('/') + 1);
+                File filepath = Environment.getExternalStorageDirectory();
+                File dir = new File(filepath.getAbsolutePath()
+                        + "/Wallpapers/");
+                dir.mkdirs();
+                String fileName = idStr;
+                File file = new File(dir, fileName);
+                FileOutputStream fos = new FileOutputStream(file);
+                bmImg.compress(Bitmap.CompressFormat.JPEG, 75, fos);
+                fos.flush();
+                fos.close();
+
+                File imageFile = file;
+                filePatrh = imageFile.getPath();
+                MediaScannerConnection.scanFile(context,
+                        new String[] { imageFile.getPath() },
+                        new String[] { "image/jpeg" }, null);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String args) {
+            // TODO Auto-generated method stub
+
+            if (bmImg == null) {
+
+                Toast.makeText(context, "Image still loading...",
+                        Toast.LENGTH_SHORT).show();
+
+                pDialog.dismiss();
+
+            }
+
+            else {
+
+                if (pDialog!=null) {
+                    if (pDialog.isShowing()) {
+                        pDialog.dismiss();
+                    }
+                }
+
+                Toast.makeText(context, "Wallpaper Successfully Saved",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+    }
+}
